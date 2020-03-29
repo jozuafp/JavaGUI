5da3c4f37a200d7b1e6c235a13b087005ac57479
jozua palandi <jozuafp@gmail.com>
2020-03-29 11:54:34 +0700

    Contoh Combo Box

---
+++ ContohJComboBox/src/contohjcombobox/ContohJComboBox.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohjcombobox;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author JW
+ */
+public class ContohJComboBox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Contoh ComboBox");
+        String pemrog[] = {"COBOL", "Pascal", "C++", "Java", "R"};
+        JComboBox cb = new JComboBox(pemrog);
+        cb.setBounds(50, 50, 90, 20);
+        f.add(cb);
+        f.setLayout(null);
+        f.setSize(500, 500);
+        f.setVisible(true);
+    }
+
+}
