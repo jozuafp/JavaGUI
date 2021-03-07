d9a8267a4e41bd38eba325f1c0f5368cea2342d1
Jozua Palandi <Jozua Palandi@LAPTOP-DHRQMOMT>
2021-03-08 01:25:40 +0700

    Try Java GUI

---
+++ InputTextGUI/src/inputtextgui/InputTextGUI.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inputtextgui;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jozua Palandi
+ */
+public class InputTextGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = JOptionPane.showInputDialog("nama : ");
+        String usia = JOptionPane.showInputDialog("usia : ");
+        int usiaXint = Integer.parseInt(usia);
+        System.out.println("Nama saya adalah " + nama);
+        System.out.println("Usia saya " + usia + " tahun");
+        System.exit(0);
+    }
+    
+}
